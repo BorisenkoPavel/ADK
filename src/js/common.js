4747748872a1b40e4d5719767beec2b9bc268e21
Павел Борисенко <pavelborisenko@list.ru>
2019-08-28 12:12:23 +0700

    commit

--- src/js/common.js
+++ src/js/common.js
@@ -7,6 +7,23 @@ $(document).ready(function () {
 
   svg4everybody({});
   
+ $('.hamburger').on('click', function(){
+   $(this).toggleClass('is-active')
+   $('.menu-content').toggleClass('is-active')
+ })
+
+ $( "#country" ).selectmenu();
+
+  // $(function() {
+  //   var lastElement = false;
+  //   $(".catalog-item__link-items > .catalog-item__link-item").each(function() {
+  //       if (lastElement && lastElement.offset().top != $(this).offset().top) {
+  //           lastElement.addClass("nobullet");
+  //       }
+  //       lastElement = $(this);
+  //   }).last().addClass("nobullet");
+  // });​
+
 
 })
 
